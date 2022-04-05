cc3e779588ad1ddd1c2d7beed097a153aadffafc
dhanya <gopal.dhanya@gmail.com>
2022-04-05 09:23:06 +0530

    event binding using $event

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,7 +12,7 @@ export class LoginComponent implements OnInit {
   acno=""
   pswd=""
 
-  database = {
+  database:any = {
     1000:{acno:1000,uname:"arun",password:1000,balance:5000},
     1001:{acno:1001,uname:"vineeth",password:1001,balance:3000},
     1002:{acno:1002,uname:"shyam",password:1002,balance:7000},
@@ -23,23 +23,39 @@ export class LoginComponent implements OnInit {
   }
 
   acnoChange(event:any){
-
    //console.log(event.target.value)
     this.acno= event.target.value
-    console.log(this.acno)
-    
+    console.log(this.acno) 
   }
 
   pswdChange(event:any){
-
     //console.log(event.target.value)
      this.pswd= event.target.value
      console.log(this.pswd)
-
    }
 
+   //login -using event binding
   login(){
-    alert("login clicked!!!")
+    
+    var acno = this.acno
+    var pswd = this.pswd
+
+    let database = this.database
+
+    if(acno in database){
+
+      if(pswd == database[acno]["password"]){
+         alert("Login successfull!!!")
+      }
+      else{
+        alert("Incorrect password!!!")
+      }
+
+    }
+    else{
+      alert("User doesnot exist!!!!")
+    }
+
   }
 
 }
